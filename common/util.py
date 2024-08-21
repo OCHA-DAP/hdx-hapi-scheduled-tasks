982b1b3c25e59d7a6d3700a51c1ef5067fac9b66
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-08-21 07:36:51 +0100

    HDX-10069 Use requests throughout and only handle JSON responses

--- common/util.py
+++ common/util.py
@@ -2,7 +2,6 @@ import json
 import time
 import os
 import requests
-from urllib import request
 
 HAPI_BASE_URL = os.getenv("HAPI_BASE_URL")
 HDX_BLUE_KEY = os.getenv("HDX_BLUE_KEY")
@@ -36,8 +35,7 @@ def fetch_data_from_hapi(query_url, limit=1000):
     """
 
     if "encode_app_identifier" in query_url:
-        with request.urlopen(query_url) as response:
-            json_response = json.loads(response.read())
+        json_response = requests.request("get", query_url).json()
 
         return json_response
 
@@ -49,31 +47,17 @@ def fetch_data_from_hapi(query_url, limit=1000):
         offset = idx * limit
         url = f"{query_url}&offset={offset}&limit={limit}"
 
-        with request.urlopen(url) as response:
-            print(f"Getting results {offset} to {offset+limit-1}")
-            print(f"{url}", flush=True)
-            encoding = response.headers.get_content_charset()
+        response = requests.request("GET", url)
+        print(f"Getting results {offset} to {offset+limit-1}")
+        print(f"{url}", flush=True)
 
-            # print(response.headers, flush=True)
-            if "output_format=json" in query_url:
-                json_response = json.loads(response.read())
+        json_response = response.json()
 
-                results.extend(json_response["data"])
-                # If the returned results are less than the limit,
-                # it's the last page
-                if len(json_response["data"]) < limit:
-                    break
-            else:
-                raw = response.read().decode(encoding)
-                csv_rows = raw.splitlines()
-                # Don't include the header line except for the first file
-                if len(results) == 0:
-                    results.extend(csv_rows)
-                else:
-                    results.extend(csv_rows[1:])
-
-                if len(csv_rows) < limit:
-                    break
+        results.extend(json_response["data"])
+        # If the returned results are less than the limit,
+        # it's the last page
+        if len(json_response["data"]) < limit:
+            break
         idx += 1
 
     print(f"Download took {time.time()-t0:0.2f} seconds", flush=True)
@@ -97,7 +81,6 @@ def fetch_data_from_ckan_api(query_url, query):
     response = requests.request("POST", query_url, headers=headers, data=payload)
     full_response_json = response.json()
     n_expected_result = full_response_json["result"]["count"]
-    # print(full_response_json, flush=True)
 
     result_length = len(full_response_json["result"]["results"])
 
